19090615db39dd05d2ad465c170581ad9040d41f
Klauber Fischer <klaubeer@gmail.com>
2026-03-12 11:50:52 -0300

    Update estado_campanha.py

--- orquestrador/estado_campanha.py
+++ orquestrador/estado_campanha.py
@@ -1,10 +1,12 @@
-
 from typing import TypedDict, Dict, Any
 
 
 class EstadoCampanha(TypedDict):
     produto: str
+
     pesquisa: Dict[str, Any]
     estrategia: Dict[str, Any]
     copy: Dict[str, Any]
     social: Dict[str, Any]
+
+    tokens_usados: int
