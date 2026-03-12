7ecc8d48b7d05ba999ae53fa523379f527df95d2
Klauber Fischer <klaubeer@gmail.com>
2026-03-12 13:12:38 -0300

    Update grafo_agentes.py

--- orquestrador/grafo_agentes.py
+++ orquestrador/grafo_agentes.py
@@ -1,11 +1,11 @@
 from langgraph.graph import StateGraph
 from orquestrador.estado_campanha import EstadoCampanha
-from agentes.diretor_criativo import diretor_criativo
 
 from agentes.agente_pesquisa import agente_pesquisa
 from agentes.agente_estrategia import agente_estrategia
 from agentes.agente_copywriter import agente_copywriter
 from agentes.agente_social_media import agente_social_media
+from agentes.diretor_criativo import diretor_criativo
 
 
 def construir_grafo():
@@ -15,13 +15,15 @@ def construir_grafo():
     fluxo.add_node("pesquisa", agente_pesquisa)
     fluxo.add_node("estrategia", agente_estrategia)
     fluxo.add_node("copywriter", agente_copywriter)
+    fluxo.add_node("diretor_criativo", diretor_criativo)
     fluxo.add_node("social", agente_social_media)
 
     fluxo.set_entry_point("pesquisa")
 
     fluxo.add_edge("pesquisa", "estrategia")
     fluxo.add_edge("estrategia", "copywriter")
-    fluxo.add_edge("copywriter", "social")
+    fluxo.add_edge("copywriter", "diretor_criativo")
+    fluxo.add_edge("diretor_criativo", "social")
 
     grafo = fluxo.compile()
 
